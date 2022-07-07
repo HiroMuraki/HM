65d7a108f685557e68c8d9c86d51cc167b6040b1
HiroMuraki <419734782@qq.com>
2022-07-07 12:39:52 +0800

    update wpf lib

---
+++ Wpf/Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HM.Wpf
+{
+    public static class Util
+    {
+        public static async Task<int> GetMouseButtonHeldTime(MouseDevice mouseDevice, int timeOut)
+        {
+            int delayMS = 10;
+            int heltTime = 0;
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(delayMS));
+                heltTime += delayMS;
+                if (mouseDevice.LeftButton != MouseButtonState.Pressed || (timeOut >= 0 && heltTime >= timeOut))
+                {
+                    return heltTime;
+                }
+            }
+        }
+    }
+}
--- Wpf/VisualTreeUtil.cs
+++ Wpf/VisualTreeUtil.cs
@@ -1,31 +1,12 @@
 ﻿using System.Security.Cryptography;
 using System.IO;
 using System;
-using System.Threading.Tasks;
-using System.Windows.Input;
 using System.Windows;
 using System.Windows.Media;
 using System.Collections.Generic;
 
 namespace HM.Wpf
 {
-    public static class Util
-    {
-        public static async Task<int> GetMouseButtonHeldTime(MouseDevice mouseDevice, int timeOut)
-        {
-            int delayMS = 10;
-            int heltTime = 0;
-            while (true)
-            {
-                await Task.Delay(TimeSpan.FromMilliseconds(delayMS));
-                heltTime += delayMS;
-                if (mouseDevice.LeftButton != MouseButtonState.Pressed || (timeOut >= 0 && heltTime >= timeOut))
-                {
-                    return heltTime;
-                }
-            }
-        }
-    }
     public static class VisualTreeUtil
     {
         public static T? FindVisualParent<T>(DependencyObject? dp) where T : DependencyObject
@@ -40,19 +21,6 @@ namespace HM.Wpf
             }
             return null;
         }
-        public static DependencyObject? FindVisualParent(DependencyObject? dp, Type targetType)
-        {
-            while (dp is not null)
-            {
-                if (dp.GetType() == targetType)
-                {
-                    return dp;
-                }
-                dp = VisualTreeHelper.GetParent(dp);
-            }
-            return null;
-        }
-
         public static T? FindVisualChild<T>(DependencyObject? obj) where T : DependencyObject
         {
             for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
@@ -65,26 +33,6 @@ namespace HM.Wpf
                 else
                 {
                     T? childOfChild = FindVisualChild<T>(child);
-                    if (childOfChild is not null)
-                    {
-                        return childOfChild;
-                    }
-                }
-            }
-            return null;
-        }
-        public static DependencyObject? FindVisualChild(DependencyObject? obj, Type targetType)
-        {
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
-            {
-                var child = VisualTreeHelper.GetChild(obj, i);
-                if (child.GetType() == targetType)
-                {
-                    return child;
-                }
-                else
-                {
-                    var childOfChild = FindVisualChild(child, targetType);
                     if (childOfChild is not null)
                     {
                         return childOfChild;
