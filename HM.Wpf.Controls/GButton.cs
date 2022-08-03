6a8db91b31558543ed893aba306f462b25b91123
HiroMuraki <419734782@qq.com>
2022-08-03 21:54:07 +0800

    add Wpf.Controls Lib

---
+++ HM.Wpf.Controls/GButton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HM.Wpf.Controls
+{
+    public class GButton : Button
+    {
+        public static readonly DependencyProperty CornerRadiusProperty =
+             DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(GButton), new PropertyMetadata(new CornerRadius(0)));
+        public static readonly DependencyProperty FocusedBackgroundProperty =
+            DependencyProperty.Register(nameof(FocusedBackground), typeof(Brush), typeof(GButton), new PropertyMetadata(null));
+
+        public CornerRadius CornerRadius
+        {
+            get
+            {
+                return (CornerRadius)GetValue(CornerRadiusProperty);
+            }
+            set
+            {
+                SetValue(CornerRadiusProperty, value);
+            }
+        }
+        public Brush FocusedBackground
+        {
+            get
+            {
+                return (Brush)GetValue(FocusedBackgroundProperty);
+            }
+            set
+            {
+                SetValue(FocusedBackgroundProperty, value);
+            }
+        }
+
+        static GButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GButton), new FrameworkPropertyMetadata(typeof(GButton)));
+        }
+    }
+}
---
+++ HM.Wpf.Controls/GSlider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HM.Wpf.Controls
+{
+    [TemplatePart(Name = "PART_TrackBorder", Type = typeof(Border))]
+    public class GSlider : Slider
+    {
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(GSlider), new PropertyMetadata(new CornerRadius(0)));
+        public static readonly DependencyProperty AccentBrushProperty =
+            DependencyProperty.Register(nameof(AccentBrush), typeof(Brush), typeof(GSlider), new PropertyMetadata(null));
+
+        public Brush AccentBrush
+        {
+            get
+            {
+                return (Brush)GetValue(AccentBrushProperty);
+            }
+            set
+            {
+                SetValue(AccentBrushProperty, value);
+            }
+        }
+        public CornerRadius CornerRadius
+        {
+            get
+            {
+                return (CornerRadius)GetValue(CornerRadiusProperty);
+            }
+            set
+            {
+                SetValue(CornerRadiusProperty, value);
+            }
+        }
+
+        static GSlider()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GSlider), new FrameworkPropertyMetadata(typeof(GSlider)));
+        }
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            ((Border)Template.FindName("PART_TrackBorder", this)).CornerRadius = new CornerRadius()
+            {
+                TopLeft = 0,
+                TopRight = CornerRadius.TopRight,
+                BottomLeft = 0,
+                BottomRight = CornerRadius.BottomRight
+            };
+        }
+    }
+}
