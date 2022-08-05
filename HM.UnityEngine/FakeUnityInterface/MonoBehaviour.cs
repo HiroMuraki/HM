369f3d88d0e4db5631603a5f0001cb80d618362b
HiroMuraki <419734782@qq.com>
2022-08-05 13:06:03 +0800

    add UnityEngile Utility Lib

---
+++ HM.UnityEngine/FakeUnityInterface/MonoBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM.UnityEngine
+{
+    public class Time
+    {
+        public static float deltaTime = -1;
+    }
+    public class WaitForEndOfFrame
+    {
+    }
+    public class Coroutine
+    {
+    }
+    public class MonoBehaviour
+    {
+        protected Coroutine StartCoroutine(IEnumerator coroutine)
+        {
+            throw new InvalidOperationException();
+        }
+        protected Coroutine StopCoroutine(Coroutine coroutine)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+    public struct Vector2
+    {
+        public float x;
+        public float y;
+
+        public Vector2(float x, float y)
+        {
+            throw new InvalidOperationException();
+        }
+        public static Vector2 operator /(Vector2 vector, float val)
+        {
+            throw new InvalidOperationException();
+        }
+        public static Vector2 operator +(Vector2 left, Vector2 right)
+        {
+            throw new InvalidOperationException();
+        }
+        public static Vector2 operator -(Vector2 left, Vector2 right)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ HM.UnityEngine/TimedListener.cs
@@ -0,0 +1,45 @@
+﻿#nullable enable
+using System.Collections;
+
+namespace HM.UnityEngine
+{
+    public class TimedListener : MonoBehaviour
+    {
+        public float Duration { get; set; }
+        public bool IsListening { get; private set; }
+
+        public void Active()
+        {
+            if (_lastTicker is not null)
+            {
+                StopCoroutine(_lastTicker);
+            }
+            _lastTicker = StartCoroutine(Ticker());
+        }
+        public void Deactive()
+        {
+            if (_lastTicker is not null)
+            {
+                StopCoroutine(_lastTicker);
+            }
+            IsListening = false;
+        }
+
+        private Coroutine? _lastTicker;
+        IEnumerator Ticker()
+        {
+            IsListening = true;
+            float timer = 0;
+            while (true)
+            {
+                if (timer >= Duration)
+                {
+                    break;
+                }
+                timer += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            IsListening = false;
+        }
+    }
+}
