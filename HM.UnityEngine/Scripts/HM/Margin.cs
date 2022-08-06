0f85cac8a7c57e8a9a7c7abfa91ea7eb4d9a0c38
HiroMuraki <419734782@qq.com>
2022-08-06 21:02:25 +0800

    update UnityEngine Util

---
+++ HM.UnityEngine/Scripts/HM/Margin.cs
@@ -0,0 +1,76 @@
+﻿#nullable enable
+using System;
+using UnityEngine;
+
+namespace HM.UnityEngine
+{
+    [Serializable]
+    public struct Margin
+    {
+        [SerializeField] float top;
+        [SerializeField] float bottom;
+        [SerializeField] float left;
+        [SerializeField] float right;
+
+        public float Top
+        {
+            get
+            {
+                return top;
+            }
+            set
+            {
+                top = value;
+            }
+        }
+        public float Bottom
+        {
+            get
+            {
+                return bottom;
+            }
+            set
+            {
+                bottom = value;
+            }
+        }
+        public float Left
+        {
+            get
+            {
+                return left;
+            }
+            set
+            {
+                left = value;
+            }
+        }
+        public float Right
+        {
+            get
+            {
+                return right;
+            }
+            set
+            {
+                right = value;
+            }
+        }
+
+        public Margin(float left, float top, float right, float bottom)
+        {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+        public Margin(float value)
+        {
+            left = value;
+            top = value;
+            right = value;
+            bottom = value;
+        }
+
+    }
+}
---
+++ HM.UnityEngine/Scripts/HM/ReactiveArea.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using UnityEngine;
+
+namespace HM.UnityEngine
+{
+    public class ReactiveArea
+    {
+        public Margin Margin { get; set; }
+        public int PixelsPerUnit { get; set; } = 100;
+        public float ScreenWidthInUnit => (float)Screen.width / PixelsPerUnit;
+        public float ScreenHeightInUnit => (float)Screen.height / PixelsPerUnit;
+        public float Width => ScreenWidthInUnit - (Margin.Left + Margin.Right);
+        public float Height => ScreenHeightInUnit - (Margin.Top + Margin.Bottom);
+        public Vector2 Center => new Vector2(Margin.Left - Margin.Right, Margin.Bottom - Margin.Top) / 2;
+        public Vector2 TopLeft => new Vector2(-ScreenWidthInUnit / 2 + Margin.Left, ScreenHeightInUnit / 2 - Margin.Top);
+        public Vector2 TopRight => new Vector2(ScreenWidthInUnit / 2 - Margin.Right, ScreenHeightInUnit / 2 - Margin.Top);
+        public Vector2 BottomLeft => new Vector2(-ScreenWidthInUnit / 2 + Margin.Left, -ScreenHeightInUnit / 2 + Margin.Bottom);
+        public Vector2 BottomRight => new Vector2(ScreenWidthInUnit / 2 - Margin.Right, -ScreenHeightInUnit / 2 + Margin.Bottom);
+
+        public ReactiveArea(Margin margin)
+        {
+            Margin = margin;
+        }
+    }
+}
